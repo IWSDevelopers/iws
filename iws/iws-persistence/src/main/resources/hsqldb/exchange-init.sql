eaa7fdda0cc5efdfa1a03ae3a649ac8280b6bc4a
Pavel Fiala <fiala_pavel@centrum.cz>
2012-07-20 02:10:52 +0200

    First exchange db draft, refs #17

---
+++ iws/iws-persistence/src/main/resources/hsqldb/exchange-init.sql
@@ -0,0 +1,101 @@
+-- Please add the initialization of all Exchange related tables here
+CREATE SEQUENCE address_sequence START 1;
+CREATE TABLE addresses {
+    id                 INTEGER      DEFAULT NextVal('address_sequence'::TEXT) NOT NULL PRIMARY KEY,
+    street1            TEXT         DEFAULT '',
+    street2            TEXT         DEFAULT '',
+    zip                TEXT         DEFAULT '',
+    city               TEXT         DEFAULT '',
+    country_id         INTEGER   NOT NULL REFERENCES countries (id) ON DELETE RESTRICT
+};
+
+CREATE SEQUENCE employer_sequence START 1;
+CREATE TABLE employers {
+    id                     INTEGER      DEFAULT NextVal('employer_sequence'::TEXT) NOT NULL PRIMARY KEY,
+    name                   TEXT         DEFAULT '',
+    address_id             INTEGER      NOT NULL REFERENCES addresses (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    workplace              TEXT         DEFAULT '',
+    website                TEXT         DEFAULT '',
+    business               TEXT         DEFAULT '',
+    responsible_person     TEXT         DEFAULT '',
+    airport                TEXT         DEFAULT '',
+    transport              TEXT         DEFAULT '',
+    employees              TEXT         DEFAULT '',
+};
+
+CREATE SEQUENCE offer_sequence  START 1;
+CREATE TABLE offers (
+    id                     INTEGER      DEFAULT NextVal('offer_sequence'::TEXT) NOT NULL PRIMARY KEY,
+    group_id               INTEGER      NOT NULL REFERENCES groups (id) ON DELETE CASCADE,
+    system_ref_no          VARCHAR(10),
+    local_ref_no           TEXT         NOT NULL,
+
+    exchange_year          INTEGER, --diferences  between years?
+    offer_year             INTEGER, --diferences  between years?
+    is_archive             INTEGER      DEFAULT 0,
+    deadline               DATE,
+    expire                 DATE,
+
+    employer_id            INTEGER      NOT NULL REFERENCES employers (id) ON DELETE CASCADE,
+    hours_weekly           REAL,
+    hours_daily            REAL,
+
+    faculty_id             INTEGER      NOT NULL --references, we have To look AT faculties TABLE,
+    faculty_other          TEXT         DEFAULT '',
+    specialization         TEXT         DEFAULT '',
+    study_completed        INTEGER, --VALUES From enum
+    study_required         VARCHAR(1), --is it used? -> delete
+    -- To be done --
+    language1              TEXT         DEFAULT '',
+    language1_level        INTEGER,
+    language1or            BOOLEAN      DEFAULT false,
+    language2              TEXT         DEFAULT '',
+    language2_level        INTEGER,
+    language2or            BOOLEAN      DEFAULT false,
+    language3              TEXT         DEFAULT '',
+    language3_level        INTEGER,
+    ----------------
+    other_requirements     TEXT,
+    training_required      TEXT         DEFAULT '',
+    gender                 INTEGER, --enum
+
+    work_kind              TEXT,
+    work_type              INTEGER, --enum
+    weeksmin               INTEGER      NOT NULL,
+    weeksmax               INTEGER      NOT NULL,
+    from_date              DATE,
+    to_date                DATE,
+    from_date2             DATE,
+    to_date2               DATE,
+    holidays_from          DATE,
+    holidays_to            DATE
+    payment                INTEGER,
+    payment_frequency      INTEGER, --enum
+    deduction              TEXT         DEFAULT '',
+
+    lodging                TEXT         DEFAULT '',
+    lodging_cost           INTEGER,
+    lodging_cost_frequency INTEGER, --enum
+    living_cost            INTEGER,
+    living_cost_frequency  INTEGER, --enum
+    canteen                BOOLEAN      DEFAULT false,
+
+    nomination_e           BOOLEAN      DEFAULT false, --to enum?
+    nomination_h           BOOLEAN      DEFAULT false, --to enum?
+    nomination_and_or      BOOLEAN      DEFAULT false, --to enum?
+    no_hard_copies         INTEGER      NOT null,
+
+    require_student        BOOLEAN      DEFAULT false,
+    comment                TEXT,
+
+    answered               TIMESTAMP,
+    status                 INTEGER, --enum
+    published              TIMESTAMP,
+    published_by           INTEGER, --i'm not sure now whether this is reference to users TABLE, need have a look at the code afternoon
+    modified               TIMESTAMP,
+    modified_by            INTEGER, --i'm NOT sure now whether this IS reference To users TABLE, need have a look AT the Code afternoon
+    created                TIMESTAMP,
+    created_by             INTEGER, --i'm not sure now whether this is reference to users TABLE, need have a look at the code afternoon
+    country_id             NOT NULL, NOT NULL REFERENCES countries (id) ON DELETE RESTRICT
+    student_id             INTEGER REFERENCES students (id) ON DELETE SET NULL
+);

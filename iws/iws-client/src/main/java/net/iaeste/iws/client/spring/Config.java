b3ab6b2dea84c3853a2d1b1a25628ea9c4b3def0
kim <kim@dawn.dk>
2012-07-23 20:23:12 +0200

    Simple Client, for external testing of the IWS.

---
+++ iws/iws-client/src/main/java/net/iaeste/iws/client/spring/Config.java
@@ -0,0 +1,67 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-client) - net.iaeste.iws.client.spring.Config
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.client.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ */
+public class Config {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.HSQL)
+                .addScript("hsqldb/init_tables.sql")
+                .addScript("hsqldb/init_views.sql")
+                .addScript("hsqldb/init_data.sql")
+                .addScript("hsqldb/exchange-init.sql")
+                .addScript("hsqldb/exchange-views.sql")
+                .addScript("hsqldb/exchange-data.sql")
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        final LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        final JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+
+        factoryBean.setPackagesToScan("net.iaeste.iws.persistence");
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        final JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+
+        return transactionManager;
+    }
+}

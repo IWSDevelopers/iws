3f2bc958e39ba0b9695da760d3a23f8444d8e997
Marko Cilimkovic <os.chilim@gmail.com>
2012-07-30 08:45:52 +0200

    Added the copyright to all java classes

--- iws-fe/src/main/java/net/iaeste/iws/fe/converter/FacultyConverter.java
+++ iws-fe/src/main/java/net/iaeste/iws/fe/converter/FacultyConverter.java
@@ -1,3 +1,18 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-fe) - net.iaeste.iws.fe.model.Offer
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+
 package net.iaeste.iws.fe.converter;
 
 import net.iaeste.iws.fe.model.Faculty;

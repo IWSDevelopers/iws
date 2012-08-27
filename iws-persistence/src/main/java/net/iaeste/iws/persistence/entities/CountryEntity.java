d8858fc902b8b4a0f8b2a60837df081335769c41
Kim Jensen <kim@dawn.dk>
2012-08-28 01:24:27 +0200

    Added new simple CountryEntity with test.

---
+++ iws-persistence/src/main/java/net/iaeste/iws/persistence/entities/CountryEntity.java
@@ -0,0 +1,92 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-persistence) - net.iaeste.iws.persistence.entities.CountryEntity
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.persistence.entities;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ */
+@NamedQueries({
+        @NamedQuery(name = "country.findAll",
+                query = "SELECT c FROM CountryEntity c"),
+        @NamedQuery(name = "country.findByName",
+                query = "select c from CountryEntity c where c.countryName = :name")
+})
+@Entity
+@Table(name = "countries")
+public class CountryEntity implements Mergeable<CountryEntity> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id = null;
+
+    @Basic
+    @Column(nullable = false, name = "country_name")
+    private String countryName = null;
+
+    /**
+     * Empty Constructor, required by JPA.
+     */
+    public CountryEntity() {
+    }
+
+    /**
+     * Default Constructor.
+     *
+     * @param countryName  The name of the Country
+     */
+    public CountryEntity(final String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setCountryName(final String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void merge(final CountryEntity obj) {
+        if (obj != null) {
+            // Only update the Id, if this is null (not persisted)
+            id = id != null ? id : obj.id;
+            countryName = obj.countryName;
+        }
+    }
+}
---
+++ iws-persistence/src/test/java/net/iaeste/iws/persistence/entities/CountryEntityTest.java
@@ -0,0 +1,77 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-persistence) - net.iaeste.iws.persistence.entities.CountryEntityTest
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.persistence.entities;
+
+import net.iaeste.iws.persistence.setup.SpringConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {SpringConfig.class})
+@TransactionConfiguration(defaultRollback = true)
+public class CountryEntityTest {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    @Transactional
+    public void testClassflow() {
+        final String countryName = "myLand";
+        final CountryEntity entity = new CountryEntity(countryName);
+
+        entityManager.persist(entity);
+        final Query query = entityManager.createNamedQuery("country.findByName");
+        query.setParameter("name", countryName);
+        final List<CountryEntity> found = query.getResultList();
+
+        assertThat(found.size(), is(1));
+        assertThat(found.get(0).getId(), is(1));
+        assertThat(found.get(0).getCountryName(), is(countryName));
+    }
+
+    @Test
+    public void testMerging() {
+        final String countryName = "myLand";
+        final Integer id = 1;
+
+        final CountryEntity original = new CountryEntity();
+        final CountryEntity merged = new CountryEntity();
+        original.setId(id);
+        original.setCountryName(countryName);
+        merged.merge(original);
+
+        assertThat(merged.getId(), is(original.getId()));
+        assertThat(merged.getCountryName(), is(original.getCountryName()));
+    }
+}

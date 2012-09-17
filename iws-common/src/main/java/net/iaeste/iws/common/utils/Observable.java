fe5268ea90f15833cf5e467f8776b64c026769fe
Kim Jensen <kim@dawn.dk>
2012-09-17 20:49:38 +0200

    Added interfaces for the Observer Design Pattern.
    Although Java is coming with the same as part of the standard, there are (from my experience) some strange issues associated with using them. Since the Pattern is so simple, and as we need it for the Notification, I've added some we can use instead.

---
+++ iws-common/src/main/java/net/iaeste/iws/common/utils/Observable.java
@@ -0,0 +1,48 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-common) - net.iaeste.iws.common.utils.Observable
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.common.utils;
+
+/**
+ * Although there already exists a Java.util implementation of the Observer
+ * design pattern, we're adding our own to provide logging/debugging mechanisms.
+ *
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ */
+public interface Observable {
+
+    /**
+     * Adds the Observer object to the list of observers to be notified by the
+     * subject.
+     *
+     * @param observer Observer object to be nofified by the subject
+     */
+    void addObserver(Observer observer);
+
+    /**
+     * Deletes an Observer object from the list of observers. This means that
+     * the observer object will no longer receive notifications by the subject.
+     *
+     * @param observer Observer object to be removed
+     */
+    void deleteObserver(Observer observer);
+
+    /**
+     * Iterates over the list of observers, and invokes their update methods,
+     * thus notifying them of the update.
+     */
+    void notifyObservers();
+}
---
+++ iws-common/src/main/java/net/iaeste/iws/common/utils/Observer.java
@@ -0,0 +1,38 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-common) - net.iaeste.iws.common.utils.Observer
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.common.utils;
+
+/**
+ * The Observer interface is used by the "observers" in the
+ * <a href="http://en.wikipedia.org/wiki/Observer_pattern">Observer Design
+ * Pattern</a>. The Observable interface is used by those classes, who wishes
+ * to become observable or "subjects".
+ *
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ * @see <a href="http://en.wikipedia.org/wiki/Observer_pattern">Observer Design Pattern</a>
+ */
+public interface Observer {
+
+    /**
+     * Whenever an observer needs to be notified of a change, this method is
+     * invoked with the subject as parameter. The method is invoked by the
+     * Observable notityObservers method.
+     *
+     * @param  subject  the observable object
+     */
+    void update(Observable subject);
+}

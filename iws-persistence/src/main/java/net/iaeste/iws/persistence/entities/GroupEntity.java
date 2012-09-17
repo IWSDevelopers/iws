249efa16380a9b5cddd82fbf4e1d37fe5f41a994
Kim Jensen <kim@dawn.dk>
2012-09-17 20:50:57 +0200

    Added primitive Group Objects.
    The Group Object is slowly becoming very, very important so to ensure that other development can continue, I've added primitive variant, that (although *not* complete) works.

---
+++ iws-persistence/src/main/java/net/iaeste/iws/persistence/entities/GroupEntity.java
@@ -0,0 +1,88 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-persistence) - net.iaeste.iws.persistence.entities.GroupEntity
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.persistence.entities;
+
+import net.iaeste.iws.persistence.monitoring.Monitored;
+import net.iaeste.iws.persistence.monitoring.MonitoringLevel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * @author Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since 1.7
+ */
+@NamedQueries({
+        @NamedQuery(
+                name = "gorup.findAll",
+                query = "select g from GroupEntity g"),
+        @NamedQuery(
+                name = "group.findById",
+                query = "select g from GroupEntity g " +
+                        "where g.id = :id")
+})
+@Entity
+@Table(name = "groups")
+@Monitored(name = "groups", level = MonitoringLevel.MARKED)
+public class GroupEntity implements IWSEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "groupname")
+    private String groupName;
+
+    /**
+     * Empty Constructor, JPA requirement.
+     */
+    public GroupEntity() {
+        id = null;
+        groupName = null;
+    }
+
+    /**
+     * Default Constructor, for creating new entity.
+     *
+     * @param groupName  Group Name
+     */
+    public GroupEntity(final String groupName) {
+        id = null;
+        this.groupName = groupName;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setGroupName(final String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+}

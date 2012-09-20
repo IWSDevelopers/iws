525766f5a023403452ea425ea6a17c1e26cc2869
Kim Jensen <kim@dawn.dk>
2012-09-20 17:10:41 +0200

    Permission Check added.
    The checks in the system to see if a user is allowed to perform a certain action is already in place, this is dealt with in the Controllers. However, the mechanism to verify if a user is permitted to access a given Object was not in place. The mechanism is simple, we compare the Group from the AuthenticationObject with the Group from the Object to be access. If not permitted, then a PermissionException is thrown.

---
+++ iws-core/src/main/java/net/iaeste/iws/core/exceptions/PermissionException.java
@@ -0,0 +1,41 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-core) - net.iaeste.iws.core.services.AccessService
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.core.exceptions;
+
+import net.iaeste.iws.api.constants.IWSConstants;
+import net.iaeste.iws.api.constants.IWSErrors;
+import net.iaeste.iws.api.exceptions.IWSException;
+
+/**
+ * @author Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since 1.7
+ */
+public final class PermissionException extends IWSException {
+
+    /** {@link IWSConstants#SERIAL_VERSION_UID}. */
+    private static final long serialVersionUID = IWSConstants.SERIAL_VERSION_UID;
+
+    /**
+     * Default Constructor, for the case where an error condition has arisen,
+     * and the only information available is the type of error, and a message
+     * describing the error.
+     *
+     * @param message  Specific message, regarding the problem
+     */
+    public PermissionException(final String message) {
+        super(IWSErrors.NOT_PERMITTED, message);
+    }
+}
---
+++ iws-core/src/main/java/net/iaeste/iws/core/services/CommonService.java
@@ -0,0 +1,41 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-core) - net.iaeste.iws.core.services.CommonService
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.core.services;
+
+import net.iaeste.iws.core.exceptions.PermissionException;
+import net.iaeste.iws.persistence.Authentication;
+import net.iaeste.iws.persistence.entities.GroupEntity;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ */
+public class CommonService {
+
+    /**
+     * Checks if the user is permitted to access the requested Object, by
+     * comparing the Owning Group for the Object. If not allowed, then a
+     * {@code PermissionException} is thrown.
+     *
+     * @param authentication Authentication Object
+     * @param group          The group to check if the user is in
+     */
+    protected void permissionCheck(final Authentication authentication, final GroupEntity group) {
+        if (!authentication.getGroup().getId().equals(group.getId())) {
+            throw new PermissionException("User is not member of the group " + group.getGroupName());
+        }
+    }
+}

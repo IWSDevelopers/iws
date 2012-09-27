a9aea30d0075cbaaa83c2db3d88fcf9697df3782
Michal Knapik <michal.knapik@iaeste.org>
2012-09-27 17:25:08 +0200

    add AbstractVerification to ProcessOfferRequest, refs #106

--- iws-api/src/main/java/net/iaeste/iws/api/requests/ProcessOfferRequest.java
+++ iws-api/src/main/java/net/iaeste/iws/api/requests/ProcessOfferRequest.java
@@ -16,7 +16,6 @@ package net.iaeste.iws.api.requests;
 
 import net.iaeste.iws.api.constants.IWSConstants;
 import net.iaeste.iws.api.dtos.Offer;
-import net.iaeste.iws.api.exceptions.VerificationException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +26,7 @@ import java.util.Map;
  * @noinspection CastToConcreteClass
  * @since   1.7
  */
-public final class ProcessOfferRequest implements Verifiable {
+public final class ProcessOfferRequest extends AbstractVerification implements Verifiable {
 
     /** {@link IWSConstants#SERIAL_VERSION_UID}. */
     private static final long serialVersionUID = IWSConstants.SERIAL_VERSION_UID;
@@ -73,18 +72,14 @@ public final class ProcessOfferRequest implements Verifiable {
      * {@inheritDoc}
      */
     @Override
-    public void verify() throws VerificationException {
-        if(offer == null) {
-            throw new VerificationException("The Offer may not be null.");
-        }
-        offer.verify();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public Map<String, String> validate() {
-        return new HashMap<String, String>(0);
+        final HashMap<String, String> validation = new HashMap<>(0);
+
+        isNotNull(validation, "offer", offer);
+        if(offer != null) {
+            addAllErrors(validation, offer.validate());
+        }
+
+        return validation;
     }
 }

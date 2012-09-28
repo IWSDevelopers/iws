42a2dc612e7f3ccbc3dc037ab0968222c84f4e71
Kim Jensen <kim@dawn.dk>
2012-09-28 15:09:16 +0200

    Started with adding corrections to DB scripts.
    Not yet done :-)

--- iws-persistence/src/main/resources/net/iaeste/iws/persistence/hsqldb/init_data.sql
+++ iws-persistence/src/main/resources/net/iaeste/iws/persistence/hsqldb/init_data.sql
@@ -1,3 +1,22 @@
+-- Standard GroupTypes
+insert into grouptypes (id, grouptype, description) values (0, 'Administration', 'The Super type, designated to all functionality needed by the system administrators, to correct data at runtime without touching the underlying system.<br /> By default, only 2 groups exists with this group type: <ul> <li><b>Private</b><br /> Certain functionality in the system is determined private initially, and thus is assigned to this group, as the only information that matters when attempting to access it, is the user. </li> <li><b>Admin</b><br /> The main admin group, for functionality in the system, which only members with this access level may use. The primary goal for this, is to correct data or reconstruct reported bugs, if they are hard to track by more conventional matters. However, all usage of this functionality may only be made with the consent of the user involved, as it may otherwise be a criminal offense since data protection laws can be violated. </li> </ul> Note; users can only be member of 1 Administration Group!');
+insert into grouptypes (id, grouptype, description) values (1, 'Members', ' All members are assigned to this type, which gives the rights to the basic functionality in the system.<br /> Each member country have a designated Members group, where all their members are added. However, as some members may not be a member of a specific country, to avoid conflicts between their work and their national organization - another group exists called "Global", for all other members. Mostly this consists of the General Secretary, Ombudsman, IDT members, etc.<br /> Note; users can only be member of 1 Members Group!');
+insert into grouptypes (id, grouptype, description) values (2, 'International', 'A number of Groups exists, which are truly "International", meaning that members of these groups may come from anywhere and there are no restrictions to this.<br /> International Groups serve very specific needs, which means that only the International Groups can post information in the system of general nature, i.e. accessible to all.<br /> The list of pre-defined International Groups include the following: <ul> <li><b>GS</b><br /> General Secretary, and assigned assistents.</li> <li><b>Board</b><br /> Members of the IAESTE A.s.b.l. Board. </li><li><b>SID</b><br /> Members who participate in the annual Seminar on IAESTE Development. </li> <li><b>IDT</b><br /> Members of the IAESTE Internet Development Team. </li> <li><b>Alumni</b><br /> The members of the IAESTE Alumni organization, formerly known as FoIN, The Friends of IAESTE Network. </li> <li><b>Jump</b><br /> Participants in the annual Jump event, a training forum for members who wishes to participate in International IAESTE work. </li> <li><b>Ombudsmand</b><br /> The IAESTE Ombudsmand.</li></ul>');
+insert into grouptypes (id, grouptype, description) values (3, 'Regional', 'Regional Groups are for a collection of Countries creating a region of the world. This can be anything like the Americas, Asia, Nordic Countries, Central European Countries, EU, etc. These types of groups have some similarities with International Groups, with some important difference. Members can only come from the region itself, and Regional Groups cannot share information for all to review.');
+insert into grouptypes (id, grouptype, description) values (4, 'National', 'All Countries have both a Members group, where all the people who are a part of the Organization in that country are listed. However, for the staff, certain other functionality is required. The National Group will make up for that.<br />  The type of functionality will consists of access to certain sections of the IntraWeb, and only some of the members of the Staff group will be allowed to join the NC''s Mailinglist.<br /> Note; users can only be member of 1 National or SAR Group!');
+insert into grouptypes (id, grouptype, description) values (5, 'SAR', 'SAR''s or Self Administrated Region, is areas where there are discussions as to the regions self reliance. An example would be Denmark, where theregions Greenland and Faroe Islands are both SAR''s, with independent governments, but still relying on the primary country''s blessing incertain areas.<br /> Note; users can only be member of 1 National or SAR Group!');
+insert into grouptypes (id, grouptype, description) values (6, 'Local', 'Local Groups are for Local Committees around the Country. Local Groups will have a National Group as parent Group.');
+insert into grouptypes (id, grouptype, description) values (7, 'Workgroup', 'For Groups, where you need only to have a common mailinglist as well as some other means of sharing information, the Workgroups will serve this purpose well.<br /> Workgroups can be assigned as a sub-group to any of the other groups.');
+insert into grouptypes (id, grouptype, description) values (8, 'Alumni', 'Alumni Group. Currently unassigned.');
+insert into grouptypes (id, grouptype, description) values (9, 'Students', 'Students Group, for allowing externals access to limited parts of the IntraWeb, currently unassigned.');
+
+-- Standard Roles
+insert into roles (id, role, description) values (0, 'Administrator', 'Super User, for all system related operations. Required for the initial setup.');
+insert into roles (id, role, description) values (1, 'Owner', 'Each group may have 1 owner, that has access to all the data and permissions.');
+insert into roles (id, role, description) values (2, 'Moderator', 'Moderators have access to all data and permissions, with the exception of being allowed to change the owner.');
+insert into roles (id, role, description) values (3, 'Member', 'Standard role for users, granted access to all data, and most common permissions that are related to administration of the Group.');
+insert into roles (id, role, description) values (4, 'Guest', 'Guests are only allowed to view or observe, they may not perform any operations, nor will be on the mailinglists.');
+
 -- Permissions from net.iaeste.iws.api.enums.Permission
 insert into permissions (id, permission) values ( 1, 'AUTHENTICATE');
 insert into permissions (id, permission) values ( 2, 'AUTHORIZE');
@@ -18,31 +37,6 @@ insert into permissions (id, permission) values (16, 'LOOKUP_FACULTIES');
 insert into permissions (id, permission) values (17, 'PROCESS_FACULTIES');
 insert into permissions (id, permission) values (18, 'PROCESS_STUDENTS');
 insert into permissions (id, permission) values (19, 'LOOKUP_STUDENTS');
-
--- GroupTypes from net.iaeste.iws.api.enums.GroupType
-insert into grouptypes (id, grouptype) values (0, 'Administration');
-insert into grouptypes (id, grouptype) values (1, 'Members');
-insert into grouptypes (id, grouptype) values (2, 'International');
-insert into grouptypes (id, grouptype) values (3, 'Regional');
-insert into grouptypes (id, grouptype) values (4, 'National');
-insert into grouptypes (id, grouptype) values (5, 'SAR');
-insert into grouptypes (id, grouptype) values (6, 'Local');
-insert into grouptypes (id, grouptype) values (7, 'Workgroup');
-insert into grouptypes (id, grouptype) values (8, 'Alumni');
-insert into grouptypes (id, grouptype) values (9, 'Students');
-
--- The Default roles should be generic, meaning that they are not associated to
--- any specific functionality or title or group. The link between a user and a
--- group is made with a role - a user can only have 1 role in a group, it is
--- not possible to have additional roles. Customization at this level is
--- achieved by giving the user a custom title.
---   Any additonally created roles will be "custom", meaning that they serve a
--- different purpose, that can be linked towards specific needs.
-insert into roles (id, role) values (0, 'Administrator');
-insert into roles (id, role) values (1, 'Owner');
-insert into roles (id, role) values (2, 'Moderator');
-insert into roles (id, role) values (3, 'Member');
-insert into roles (id, role) values (4, 'Guest');
 
 
 

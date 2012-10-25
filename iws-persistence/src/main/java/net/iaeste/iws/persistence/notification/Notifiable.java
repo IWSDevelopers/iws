b716b6e29135518c7279ce1adb15560114ab69c7
Kim Jensen <kim@dawn.dk>
2012-10-25 15:37:42 +0200

    Added Notification Interfaces (refs #134)
    The Notification should work via Interfaces, so it is possible to mock it away or create alternative implementations that should be used.

---
+++ iws-persistence/src/main/java/net/iaeste/iws/persistence/notification/Notifiable.java
@@ -0,0 +1,23 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-core) - net.iaeste.iws.persistence.notification.Notifiable
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.persistence.notification;
+
+/**
+ * @author Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since 1.7
+ */
+public interface Notifiable {
+}

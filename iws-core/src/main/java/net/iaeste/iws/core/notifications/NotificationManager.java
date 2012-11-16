6e2cab02326352de5816390c08920dd83c5d94ed
Pavel Fiala <pavel.fiala@iaeste.cz>
2012-11-16 23:28:15 +0100

    Typo change in notification (refs #134)
    Correct copyright after renaming

--- iws-core/src/main/java/net/iaeste/iws/core/notifications/NotificationManager.java
+++ iws-core/src/main/java/net/iaeste/iws/core/notifications/NotificationManager.java
@@ -2,7 +2,7 @@
  * =============================================================================
  * Copyright 1998-2012, IAESTE Internet Development Team. All rights reserved.
  * -----------------------------------------------------------------------------
- * Project: IntraWeb Services (iws-core) - net.iaeste.iws.core.notifications.NotificationsCenter
+ * Project: IntraWeb Services (iws-core) - net.iaeste.iws.core.notifications.NotificationManager
  * -----------------------------------------------------------------------------
  * This software is provided by the members of the IAESTE Internet Development
  * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be

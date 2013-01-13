aa7316ee2668769097d7e7b6f3d68383c3adcd26
Pavel Fiala <pavel.fiala@iaeste.cz>
2013-01-13 21:04:19 +0100

    Small typo correction

--- iws-api/src/main/java/net/iaeste/iws/api/requests/UserGroupAssignmentRequest.java
+++ iws-api/src/main/java/net/iaeste/iws/api/requests/UserGroupAssignmentRequest.java
@@ -32,7 +32,7 @@ public final class UserGroupAssignmentRequest extends AbstractVerification {
     /** {@link IWSConstants#SERIAL_VERSION_UID}. */
     private static final long serialVersionUID = IWSConstants.SERIAL_VERSION_UID;
 
-    /** The user that ill be added to a group. **/
+    /** The user that will be added to a group. **/
     private User user = null;
 
     /** The group to which the user will be added.**/

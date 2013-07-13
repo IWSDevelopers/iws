546f5c52680504db587387d7a86be221a73e5094
Kim Jensen <kim@dawn.dk>
2013-07-13 23:00:19 +0200

    New Process Employer Request Object.

---
+++ iws-api/src/main/java/net/iaeste/iws/api/requests/exchange/ProcessEmployerRequest.java
@@ -0,0 +1,88 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2013, IAESTE Internet Development Team. All rights reserved.
+ * -----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-api) - net.iaeste.iws.api.requests.exchange.ProcessEmployerRequest
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.api.requests.exchange;
+
+import net.iaeste.iws.api.constants.IWSConstants;
+import net.iaeste.iws.api.dtos.exchange.Employer;
+import net.iaeste.iws.api.util.AbstractVerification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   1.7
+ */
+public final class ProcessEmployerRequest extends AbstractVerification {
+
+    /** {@link IWSConstants#SERIAL_VERSION_UID}. */
+    private static final long serialVersionUID = IWSConstants.SERIAL_VERSION_UID;
+
+    private Employer employer = null;
+
+    // =========================================================================
+    // Object Constructors
+    // =========================================================================
+
+    /**
+     * Empty Constructor, to use if the setters are invoked. This is required
+     * for WebServices to work properly.
+     */
+    public ProcessEmployerRequest() {
+        employer = null;
+    }
+
+    /**
+     * Default Constructor, sets the Employer to be processed. If the Employer
+     * exists, it will be updated otherwise a new Employer will be created.
+     *
+     * @param employer object to create or update
+     */
+    public ProcessEmployerRequest(final Employer employer) {
+        setEmployer(employer);
+    }
+
+    // =========================================================================
+    // Standard Setters & Getters
+    // =========================================================================
+
+    public void setEmployer(final Employer employer) {
+        ensureNotNullAndVerifiable("employer", employer);
+
+        this.employer = new Employer(employer);
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+
+    // =========================================================================
+    // Standard Request Methods
+    // =========================================================================
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Map<String, String> validate() {
+        final Map<String, String> validation = new HashMap<>(0);
+
+        isVerifiable(validation, "employer", employer);
+
+        return validation;
+    }
+}

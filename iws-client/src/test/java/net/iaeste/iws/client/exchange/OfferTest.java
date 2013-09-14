6294a3b64a525f90da7ccfa89c4203676172625b
Kim Jensen <kim@dawn.dk>
2013-09-14 11:47:17 +0200

    Two different empty methods were statically imported, causing test problems (refs #333).

--- iws-client/src/test/java/net/iaeste/iws/client/exchange/OfferTest.java
+++ iws-client/src/test/java/net/iaeste/iws/client/exchange/OfferTest.java
@@ -14,14 +14,13 @@
  */
 package net.iaeste.iws.client.exchange;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
-import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 

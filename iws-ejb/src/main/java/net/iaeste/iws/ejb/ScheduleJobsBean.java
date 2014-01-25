b588bcfd14a0020ea31964677c48687911655709
Kim Jensen <kim@dawn.dk>
2014-01-25 21:28:31 +0100

    Review of Scheduler Bean (refs #333).

--- iws-ejb/src/main/java/net/iaeste/iws/ejb/ScheduleJobsBean.java
+++ iws-ejb/src/main/java/net/iaeste/iws/ejb/ScheduleJobsBean.java
@@ -12,21 +12,16 @@
  * cannot be held legally responsible for any problems the software may cause.
  * =============================================================================
  */
-
 package net.iaeste.iws.ejb;
 
 import net.iaeste.iws.api.enums.exchange.OfferState;
 import net.iaeste.iws.api.exceptions.IWSException;
-import net.iaeste.iws.ejb.notifications.NotificationManager;
-import net.iaeste.iws.ejb.notifications.NotificationMessageGenerator;
-import net.iaeste.iws.ejb.notifications.NotificationMessageGeneratorFreemarker;
 import net.iaeste.iws.persistence.AccessDao;
 import net.iaeste.iws.persistence.ExchangeDao;
 import net.iaeste.iws.persistence.entities.exchange.OfferEntity;
 import net.iaeste.iws.persistence.entities.exchange.OfferGroupEntity;
 import net.iaeste.iws.persistence.jpa.AccessJpaDao;
 import net.iaeste.iws.persistence.jpa.ExchangeJpaDao;
-import net.iaeste.iws.persistence.jpa.NotificationJpaDao;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,15 +46,17 @@ import java.util.Map;
  */
 @Singleton
 public class ScheduleJobsBean {
+
     private static final Logger log = LoggerFactory.getLogger(ScheduleJobsBean.class);
 
     private EntityManager iwsEntityManager = null;
     private AccessDao accessDao = null;
     private ExchangeDao exchangeDao = null;
+
     @Resource
     private TimerService timerService;
-    private final Object LOCK = new Object();
 
+    private final Object lock = new Object();
     private boolean processExpiredOfferIsRunning = false;
 
     /**
@@ -80,56 +77,93 @@ public class ScheduleJobsBean {
     public void postConstruct() {
         log.info("post construct");
         accessDao = new AccessJpaDao(iwsEntityManager);
-        exchangeDao =new ExchangeJpaDao(iwsEntityManager);
+        exchangeDao = new ExchangeJpaDao(iwsEntityManager);
     }
 
     //for local testing
     //@Schedule(second = "*/30", minute = "*", hour = "*", info="Every 30 seconds")
     //for server
-    @Schedule(second = "0", minute = "1", hour = "0", info="Every day at 0:01 AM (server time)")
+    @Schedule(second = "0", minute = "1", hour = "0", info = "Every day at 0:01 AM (server time)")
     private void processExpiredOffers() {
-        //TODO remove log messages when the processing works correctly, i.e. after testing. Or, leave it?
+        // Leaving the log, it is generally useful to have this information
         log.info("processExpiredOffers started at " + new DateTime());
         final boolean run;
-        synchronized (LOCK) {
-            run = !processExpiredOfferIsRunning;
+
+        // Construct is not thread-safe. You're testing and setting the key in
+        // two different sync blocks, meaning that it is possible for different
+        // threads to invoke the same logid You should only have 2 sync blocks,
+        // one before the running and one after, like so:
+        synchronized (lock) {
+            if (!processExpiredOfferIsRunning) {
+                run = true;
+                processExpiredOfferIsRunning = true;
+            } else {
+                run = false;
+            }
         }
 
         if (run) {
-            synchronized (LOCK) {
-                processExpiredOfferIsRunning = true;
-            }
+            // We invcoke the logic in a try-finally block, so we can switch of
+            // the processing regardlessly of the outcome
 
             try {
-                final List<OfferEntity> offers = exchangeDao.findExpiredOffers(new Date());
-                final List<Long> ids = new ArrayList<>(offers.size());
-                for (final OfferEntity offer : offers) {
-                    ids.add(offer.getId());
+                // Now we invoke the actual logic, outside of the sync block,
+                // as to not block anything
+                runProcessing();
+            } finally {
+                // Now the worker has completed the job, we can set the
+                // processing flag back to false, so another instance can start
+                synchronized (lock) {
+                    processExpiredOfferIsRunning = false;
                 }
+            }
+        }
+        //// Following is the incorrect sync construct
+        //synchronized (lock) {
+        //    run = !processExpiredOfferIsRunning;
+        //}
+        //
+        //if (run) {
+        //    synchronized (lock) {
+        //        processExpiredOfferIsRunning = true;
+        //    }
+        //
+        //    runProcessing();
+        //
+        //    synchronized (lock) {
+        //        processExpiredOfferIsRunning = false;
+        //    }
+        //}
 
-                final List<OfferGroupEntity> offerGroups = exchangeDao.findInfoForSharedOffers(ids);
-                final Map<Long, List<OfferGroupEntity>> sharingInfo = prepareOfferGroupMap(ids, offerGroups);
+        log.info("processExpiredOffers ended at " + new DateTime());
+    }
 
-                for (final OfferEntity offer : offers) {
-                    offer.setStatus(OfferState.EXPIRED);
-                    exchangeDao.persist(offer);
+    private void runProcessing() {
+        try {
+            final List<OfferEntity> offers = exchangeDao.findExpiredOffers(new Date());
+            final List<Long> ids = new ArrayList<>(offers.size());
+            for (final OfferEntity offer : offers) {
+                ids.add(offer.getId());
+            }
 
-                    for (final OfferGroupEntity offerGroup : sharingInfo.get(offer.getId())) {
-                        if (offerGroup.getHasApplication()) {
-                            offerGroup.setStatus(OfferState.CLOSED);
-                            exchangeDao.persist(offerGroup);
-                        } else {
-                            iwsEntityManager.remove(offerGroup);
-                        }
+            final List<OfferGroupEntity> offerGroups = exchangeDao.findInfoForSharedOffers(ids);
+            final Map<Long, List<OfferGroupEntity>> sharingInfo = prepareOfferGroupMap(ids, offerGroups);
+
+            for (final OfferEntity offer : offers) {
+                offer.setStatus(OfferState.EXPIRED);
+                exchangeDao.persist(offer);
+
+                for (final OfferGroupEntity offerGroup : sharingInfo.get(offer.getId())) {
+                    if (offerGroup.getHasApplication()) {
+                        offerGroup.setStatus(OfferState.CLOSED);
+                        exchangeDao.persist(offerGroup);
+                    } else {
+                        iwsEntityManager.remove(offerGroup);
                     }
                 }
-            } catch (IllegalArgumentException|IWSException e) {
-                log.error("Error in processing expired offers", e);
             }
-
-            synchronized (LOCK) {
-                processExpiredOfferIsRunning = false;
-            }
+        } catch (IllegalArgumentException | IWSException e) {
+            log.error("Error in processing expired offers", e);
         }
     }
 

3a08e22b677b9577d54179058368cfd7c6f8d133
Kim Jensen <kim@dawn.dk>
2014-02-10 17:02:47 +0100

    Added little test to help generate passwords.

---
+++ iws-common/src/test/java/net/iaeste/iws/common/utils/GeneratePasswordTest.java
@@ -0,0 +1,40 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2014, IAESTE Internet Development Team. All rights reserved.
+ * ----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-common) - net.iaeste.iws.common.utils.GeneratePasswordTest
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.common.utils;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   IWS 1.0
+ */
+@Ignore("This test is purely to help generate new passwords for IWS Accounts.")
+public final class GeneratePasswordTest {
+
+    private static final String info = "New Password for user %s password is '%s' encrypted = %s with salt %s";
+
+    @Test
+    public void testGeneratePassword() {
+        final String username = "";
+        final String salt = UUID.randomUUID().toString();
+        final String password = PasswordGenerator.generatePassword();
+        final String generateHashedPassword = HashcodeGenerator.generateHash(password, salt);
+        System.out.println(String.format(info, username, password, generateHashedPassword, salt));
+    }
+}

f3048c0299c2edf7b79af67b3498d7b4cdcb9bd5
Kim Jensen <kim@dawn.dk>
2014-04-25 12:48:53 +0200

    Added alias for IAESTE Bolivia, IB.

--- iws-migrate/src/main/java/net/iaeste/iws/migrate/migrators/MailMigrator.java
+++ iws-migrate/src/main/java/net/iaeste/iws/migrate/migrators/MailMigrator.java
@@ -78,6 +78,7 @@ public class MailMigrator implements Migrator<IW3UsersEntity> {
             { "India@iaeste.org", "India_KU@iaeste.org" },
             { "India@iaeste.org", "India_MIT@iaeste.org" },
             { "Bangladesh_Afzal_Management@iaeste.org", "bangladeshafm@iaeste.org" },
+            { "bolivia_ib@iaeste.org", "boliviaib@iaeste.org" },
             { "idt@iaeste.net", "idt.members@iaeste.net"}};
 
     @Autowired

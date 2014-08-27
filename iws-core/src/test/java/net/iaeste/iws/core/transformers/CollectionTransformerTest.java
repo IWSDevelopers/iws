1345893847451f653aedfa0d44773a390d8b92e0
Martin Eisfeld <martin.eisfeld@gmx.net>
2014-08-27 22:47:37 +0200

    revert unwanted commit

--- iws-core/src/test/java/net/iaeste/iws/core/transformers/CollectionTransformerTest.java
+++ iws-core/src/test/java/net/iaeste/iws/core/transformers/CollectionTransformerTest.java
@@ -18,7 +18,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import net.iaeste.iws.api.enums.Language;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -96,7 +95,6 @@ public class CollectionTransformerTest {
     }
 
     @Test
-    @Ignore
     public void testExplodeOneItemStringTicket873() {
         final String[] strings = { "str1" };
         final String stringToExplode = strings[0];

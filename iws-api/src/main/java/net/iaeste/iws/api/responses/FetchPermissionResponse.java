23fa8e457914dfd6e58af4a96707b346f6fac144
Kim Jensen <kim@dawn.dk>
2014-08-28 09:44:27 +0200

    Added Permission functionality (refs #787).
    I have added some functionality to help with checking if a User is permitted to perform a given Action. I haven't had time to write tests for the methods, but they will hopefully give some ideas about how to do things correctly.
    The former way of working with Permissions in IW4 is terrible - with this functionality, we can remove most of the crap from the system, and disregard the Role or Group specific checks and instead look at the Permissions, as intended.

--- iws-api/src/main/java/net/iaeste/iws/api/responses/FetchPermissionResponse.java
+++ iws-api/src/main/java/net/iaeste/iws/api/responses/FetchPermissionResponse.java
@@ -17,10 +17,16 @@ package net.iaeste.iws.api.responses;
 import net.iaeste.iws.api.constants.IWSConstants;
 import net.iaeste.iws.api.constants.IWSError;
 import net.iaeste.iws.api.dtos.Authorization;
+import net.iaeste.iws.api.dtos.Group;
+import net.iaeste.iws.api.dtos.Role;
+import net.iaeste.iws.api.enums.GroupType;
+import net.iaeste.iws.api.enums.Permission;
 import net.iaeste.iws.api.util.AbstractFallible;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Listing of Permission for a user.
@@ -35,6 +41,7 @@ public final class FetchPermissionResponse extends AbstractFallible {
     private static final long serialVersionUID = IWSConstants.SERIAL_VERSION_UID;
     private String userId;
     private List<Authorization> authorizations;
+    private Map<Permission, List<Group>> permissionMap = null;
 
     // =========================================================================
     // Object Constructors
@@ -90,6 +97,115 @@ public final class FetchPermissionResponse extends AbstractFallible {
 
     public List<Authorization> getAuthorizations() {
         return authorizations;
+    }
+
+    // =========================================================================
+    // Additional helper method sto ease the work with finding permissions
+    // =========================================================================
+
+    /**
+     * Checks if a User is allowed to perform a given Action. If so, then a True
+     * is returned, otherwise a false.
+     *
+     * @param permission Permission to check if the User has
+     * @return True if User is permitted, otherwise false
+     */
+    public Boolean hasPermission(final Permission permission) {
+        return !getGroups(permission).isEmpty();
+    }
+
+    /**
+     * Returns the Group, with the given Id, if it also has the Permission
+     * requested. If not, then a null is returned.
+     *
+     * @param permission Permission to match the Groups for
+     * @param groupId    The Id of the Group to match
+     * @return Group with the given Criterias or null
+     */
+    public Group getGroup(final Permission permission, final String groupId) {
+        Group found = null;
+
+        for (final Group group : getGroups(permission)) {
+            if (group.getGroupId().equals(groupId)) {
+                found = group;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    /**
+     * Returns a List of all Groups, which have the given GroupType. For some
+     * GroupTypes, such as Members or National the user may only belong to one,
+     * but for others like Local Committee, International or WorkGroup, the User
+     * may belong to more.<br />
+     *   If no Groups match the Criterias, then an empty list is returned.
+     *
+     * @param permission Permission to match the Groups for
+     * @param type       GroupType to match the Groups for
+     * @return List of all Groups matching the given Type & Permission
+     */
+    public List<Group> getGroups(final Permission permission, final GroupType type) {
+        final List<Group> found = new ArrayList<>();
+
+        for (final Group group : getGroups(permission)) {
+            if (group.getGroupType() == type) {
+                found.add(group);
+            }
+        }
+
+        return found;
+    }
+
+    /**
+     *
+     * @param permission
+     * @return
+     */
+    public List<Group> getGroups(final Permission permission) {
+        final List<Group> groups;
+
+        if (permissionMap.containsKey(permission)) {
+            groups = permissionMap.get(permission);
+        } else {
+            groups = new ArrayList<>(0);
+        }
+
+        return groups;
+    }
+
+    /**
+     * This method converts the Authorizations Object into a more usable mapping
+     * of the Users permissions, by returning a Map using the individual
+     * Permissions as keys and having a list of Groups, which the user belongs
+     * to.<br />
+     *   By having this list, it is possible to quickly extract the actually
+     * required information such as if a person is allowed to perform a given
+     * action. The Actions are listed in the description of each Permission in
+     * the Permission enum.
+     *
+     * @return Map with Permissions and Groups
+     */
+    public Map<Permission, List<Group>> convertPermissions() {
+        if (permissionMap == null) {
+            permissionMap = new EnumMap<>(Permission.class);
+
+            for (Authorization authorization : authorizations) {
+                final Role role = authorization.getUserGroup().getRole();
+                final Group group = authorization.getUserGroup().getGroup();
+
+                for (Permission permission : role.getPermissions()) {
+                    if (!permissionMap.containsKey(permission)) {
+                        final List<Group> groups = new ArrayList<>(1);
+                        groups.add(group);
+                    }
+                    permissionMap.get(permission).add(group);
+                }
+            }
+        }
+
+        return permissionMap;
     }
 
     // =========================================================================

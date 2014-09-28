5269c3fa77bbb8a431809df5cb5478bf58ceb909
Khaled Sharif <kldsrf@gmail.com>
2014-09-28 21:05:41 +0200

    Allows the user to correctly change the number of weekly working days in an employer/offer, refs #734.

--- iws-api/src/main/java/net/iaeste/iws/api/dtos/exchange/Employer.java
+++ iws-api/src/main/java/net/iaeste/iws/api/dtos/exchange/Employer.java
@@ -50,6 +50,7 @@ public final class Employer extends AbstractVerification {
     private Boolean canteen = null;
     private String nearestAirport = null;
     private String nearestPublicTransport = null;
+    private Integer numberOfWorkingDays = null;
 
     // =========================================================================
     // Object Constructors
@@ -80,6 +81,7 @@ public final class Employer extends AbstractVerification {
             canteen = employer.canteen;
             nearestAirport = employer.nearestAirport;
             nearestPublicTransport = employer.nearestPublicTransport;
+            numberOfWorkingDays = employer.numberOfWorkingDays;
         }
     }
 
@@ -316,6 +318,16 @@ public final class Employer extends AbstractVerification {
 
     public String getNearestPublicTransport() {
         return nearestPublicTransport;
+    }
+
+    public void setNumberOfWorkingDays(final Integer numberOfWorkingDays)
+    {
+        this.numberOfWorkingDays = numberOfWorkingDays;
+    }
+
+    public Integer getNumberOfWorkingDays ()
+    {
+        return this.numberOfWorkingDays;
     }
 
     // =========================================================================

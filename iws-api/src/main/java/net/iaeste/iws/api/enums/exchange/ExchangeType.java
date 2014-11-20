c480a6d117adf7bfdc446a04c6d9bb0f6fa86c11
Kim Jensen <kim@dawn.dk>
2014-11-20 21:57:12 +0100

    Added new Offer & Exchange Types.
    After a lengthy discussion with various Committees, it was a wish to have the Type of Offer (Limited, Reserved or Other) as well as a type of Exchange (AC, IntraWeb, COBE) added. So it is possible to change either the Offer Type or Exchange Type, without affecting the Reference Number.

---
+++ iws-api/src/main/java/net/iaeste/iws/api/enums/exchange/ExchangeType.java
@@ -0,0 +1,41 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2014, IAESTE Internet Development Team. All rights reserved.
+ * ----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-api) - net.iaeste.iws.api.enums.exchange.ExchangeType
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.api.enums.exchange;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   ISW 1.1
+ */
+public enum ExchangeType {
+
+    IW("IntraWeb"),
+    AC("Annual Conference"),
+    COBE("COBE");
+
+    // =========================================================================
+    // Private Constructor & functionality
+    // =========================================================================
+
+    private final String description;
+
+    ExchangeType(final String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ iws-api/src/main/java/net/iaeste/iws/api/enums/exchange/OfferType.java
@@ -0,0 +1,56 @@
+/*
+ * =============================================================================
+ * Copyright 1998-2014, IAESTE Internet Development Team. All rights reserved.
+ * ----------------------------------------------------------------------------
+ * Project: IntraWeb Services (iws-api) - net.iaeste.iws.api.enums.exchange.OfferType
+ * -----------------------------------------------------------------------------
+ * This software is provided by the members of the IAESTE Internet Development
+ * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
+ * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ *
+ * This software is provided "as is"; the IDT or individuals within the IDT
+ * cannot be held legally responsible for any problems the software may cause.
+ * =============================================================================
+ */
+package net.iaeste.iws.api.enums.exchange;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+/**
+ * @author  Kim Jensen / last $Author:$
+ * @version $Revision:$ / $Date:$
+ * @since   IWS 1.1
+ */
+public enum OfferType {
+
+    LIMITED("Limited", "L", EnumSet.of(ExchangeType.AC, ExchangeType.IW, ExchangeType.COBE)),
+    RESERVED("Reserved", "R", EnumSet.of(ExchangeType.AC, ExchangeType.IW)),
+    OPEN("Open", null, EnumSet.of(ExchangeType.AC, ExchangeType.IW, ExchangeType.COBE));
+
+    // =========================================================================
+    // Private Constructor & functionality
+    // =========================================================================
+
+    private final String description;
+    private final String letter;
+    private final Set<ExchangeType> exchangeTypes;
+
+    OfferType(final String description, final String letter, final Set<ExchangeType> exchangeTypes) {
+        this.description = description;
+        this.letter = letter;
+        this.exchangeTypes = exchangeTypes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public Set<ExchangeType> getExchangeTypes() {
+        return exchangeTypes;
+    }
+}

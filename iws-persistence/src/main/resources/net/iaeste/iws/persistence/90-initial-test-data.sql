53a2126a375812f7ffcd6f5c6b0ce87455559c3e
Kim Jensen <kim@dawn.dk>
2015-10-13 09:36:47 +0200

    Minor corrections to the WSDL files.

--- iws-persistence/src/main/resources/net/iaeste/iws/persistence/90-initial-test-data.sql
+++ iws-persistence/src/main/resources/net/iaeste/iws/persistence/90-initial-test-data.sql
@@ -820,26 +820,27 @@ update groups set status = 'SUSPENDED' where id = 6;
 -- Root Folder eid = afec3bc0-296b-4bf2-8a9e-c2d7b74e93a0
 -- Make sure that we have at least 3 Folder depth, with last being public, middle or first being private
 -- Folder Structure for our tests:
---  Root
---    - Board Minutes PUBLIC
---       - AC PUBLIC
---          - 2014 PUBLIC
---       - Finances PROTECTED
---          - 2014 PUBLIC
---          - 2015 PROTECTED
---    - SID 2014 PUBLIC
---       - Administration PUBLIC
---       - IWUG PUBLIC
---    - SID 2015 PROTECTED
---       - Administration PUBLIC
---       - IWUG PUBLIC
---    - NC Tunisia Travel Documents PUBLIC
---       - 2015 PUBLIC
---          - EU PUBLIC
---          - Other PUBLIC
---       - 2014 PROTECTED
---          - EU PUBLIC
---          - Other PUBLIC
+--  Root                                     EId: "afec3bc0-296b-4bf2-8a9e-c2d7b74e93a0"
+--    - Board Minutes PUBLIC                 EId: "6adc3bac-c85c-4dfa-b9d8-3d9b848314af"
+--       - AC PUBLIC                         EId: "c9bde21a-011e-42e4-8985-b1da95c0fbdf"
+--          - 2014 PUBLIC                    EId: "af0a0c34-7037-4153-a707-65a1ed0b380b"
+--       - Finances PROTECTED                EId: "b54a005d-d9cd-4456-9263-7ab3833ab303"
+--          - 2014 PUBLIC                    EId: "4126e3c4-2e79-4ee1-a73f-a3ab5f9e52a5"
+--          - 2015 PROTECTED                 EId: "9dd06a36-4e02-4a7d-9a46-da78648eaaae"
+--    - SID 2014 PUBLIC                      EId: "d8d6d7eb-754a-4e3b-8d4d-7657339096d1"
+--       - Administration PUBLIC             EId: "01749318-9f20-4369-bd05-555c18e643ad"
+--       - IWUG PUBLIC                       EId: "888f6c83-fcae-4e4a-9f8c-4fb02c2ec8e2"
+--    - SID 2015 PROTECTED                   EId: "60ba5c47-3dce-41a1-94f9-e0dafe2f717a"
+--       - Administration PUBLIC             EId: "a4fa7715-3a10-470f-8538-e8ee13666819"
+--       - IWUG PUBLIC                       EId: "f50bed53-7295-4b91-8746-78136df0a189"
+--    - NC Tunisia Travel Documents PUBLIC   EId: "94a6d74f-c510-4302-83fd-cd79ea8a4c9a"
+--       - 2015 PUBLIC                       EId: "b0ed8121-f359-4bc5-8ce8-bc5c4a7de536"
+--          - EU PUBLIC                      EId: "ed5e0487-0605-428d-b40c-78e88df85d8d"
+--          - Other PUBLIC                   EId: "0328281b-cf80-4c7a-8ad7-04721eaa45aa"
+--       - 2014 PROTECTED                    EId: "df7a45f5-2aed-4529-a527-488ad538e03a"
+--          - EU PUBLIC                      EId: "30a460be-641e-4cb9-a336-5eab00c19c3a"
+--          - Other PUBLIC                   EId: "bb7953a8-8b7a-43a2-84fe-dca74291abd3"
+--
 -- For all of the above, we need a sample file also. This will have data stored
 -- in a new table as mentioned above. As the database is configured in production
 -- to use data replication, it is a more safe and reliable mechanism to use this,

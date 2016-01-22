32621ce8e88aa5f92491dd453b9a207b3632600c
Kim Jensen <kim@dawn.dk>
2016-01-22 09:02:07 +0100

    Fixed problem with reading templates.
    The error with using NIO seems obvious in retrospect. NIO relies on file path's and can thus not work with files in a JAR! Instead, the ancient Scanner trick is used. Strange that nobody have managed to introduce a better reading of resources than a trick which is over a decade old!
    JUnit tests of this all run through clean, and testing with sending using a local WildFly configured for sending also shows that it works.

--- iws-ejb/src/main/java/net/iaeste/iws/ejb/notifications/MessageGenerator.java
+++ iws-ejb/src/main/java/net/iaeste/iws/ejb/notifications/MessageGenerator.java
@@ -23,15 +23,12 @@ import net.iaeste.iws.common.notification.NotificationType;
 import net.iaeste.iws.ejb.emails.MessageField;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * @author  Kim Jensen / last $Author:$
@@ -43,6 +40,7 @@ public final class MessageGenerator {
     private static final String DIR = "/templates/";
     private static final String MESSAGE = "Message.tpl";
     private static final String SUBJECT = "Subject.tpl";
+    private static final Pattern PATTER_NEWLINE = Pattern.compile("\\n");
 
     private final Settings settings;
 
@@ -81,12 +79,12 @@ public final class MessageGenerator {
         final Map<MessageField, String> result = new EnumMap<>(MessageField.class);
 
         try {
-            final String subjectTemplate = readTemplate(template + SUBJECT).replaceAll("\\n", "");
+            final String subjectTemplate = PATTER_NEWLINE.matcher(readTemplate(template + SUBJECT)).replaceAll("");
             final String messageTemplate = readTemplate(template + MESSAGE);
 
             result.put(MessageField.SUBJECT, parseTemplate(subjectTemplate, parseMap));
             result.put(MessageField.MESSAGE, parseTemplate(messageTemplate, parseMap));
-        } catch (IOException | URISyntaxException | RuntimeException e) {
+        } catch (IOException | RuntimeException e) {
             throw new IWSException(IWSErrors.ERROR, "Error during processing template", e);
         }
 
@@ -95,21 +93,27 @@ public final class MessageGenerator {
 
     /**
      * Simplified template reader, which is simply reading the content byte for
-     * byte and returning an IWS encoded String of it.
+     * byte and returning it.
      *
      * @param name The name of the template to read
      * @return Template
      * @throws IOException if unable to read the template
-     * @throws URISyntaxException if the template path or name is invalid
      */
-    private static String readTemplate(final String name) throws IOException, URISyntaxException {
-        final URL url = MessageGenerator.class.getResource(DIR + name);
-        final Path path = Paths.get(url.toURI());
-
-        // All data inside the IWS is stored in Latin9 (ISO-8859-15), however
-        // our templates are written by us and stored as UTF-8, so we're
-        // reading them into this character set.
-        return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+    private static String readTemplate(final String name) throws IOException {
+        // Third iteration of the template reader. NIO won't work, as the
+        // default Java VFS for dealing with Zip files (JAR's) is rather bad.
+        // And the Path Object cannot differentiate between Files & Jar's.
+        // Using a Stream, which is then the only option left - requires that
+        // the lines must be correctly read, but using a buffered reader will
+        // result in the loss of newline information. Adding a third party
+        // library to deal with this simply thing seems is overkill. So
+        // instead, the old Scanner trick is applied.
+        // See: http://web.archive.org/web/20140531042945/https://weblogs.java.net/blog/pat/archive/2004/10/stupid_scanner_1.html
+        try (InputStream stream = MessageGenerator.class.getResourceAsStream(DIR + name);
+             Scanner streamScanner = new Scanner(stream);
+             Scanner scanner = streamScanner.useDelimiter("\\A")) {
+            return scanner.next();
+        }
     }
 
     private Map<String, String> generateParseMap(final Map<NotificationField, String> input) {

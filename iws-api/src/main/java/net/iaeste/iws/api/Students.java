20c150a761cc89cee16377c2437dd6e01b307c7a
Kim Jensen <kim@dawn.dk>
2016-02-22 22:17:26 +0100

    Relicensed IWS to APL 2.0.
    The developers who have a stake in the current version of the IWS, have all agreed to move the IWS to the Apache License, Version 2.0 (APL 2.0). Thus making the IWS an Open Source Project.
    Please note, that it is not being relicensed as an Open Source Project in the hope that it will attract more developers - it is done to open up the process of development for others and make it more democratic.

--- iws-api/src/main/java/net/iaeste/iws/api/Students.java
+++ iws-api/src/main/java/net/iaeste/iws/api/Students.java
@@ -1,16 +1,19 @@
 /*
- * =============================================================================
- * Copyright 1998-2016, IAESTE Internet Development Team. All rights reserved.
- * ----------------------------------------------------------------------------
- * Project: IntraWeb Services (iws-api) - net.iaeste.iws.api.Students
- * -----------------------------------------------------------------------------
- * This software is provided by the members of the IAESTE Internet Development
- * Team (IDT) to IAESTE A.s.b.l. It is for internal use only and may not be
- * redistributed. IAESTE A.s.b.l. is not permitted to sell this software.
+ * Licensed to IAESTE A.s.b.l. (IAESTE) under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership. The Authors
+ * (See the AUTHORS file distributed with this work) licenses this file to
+ * You under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a
+ * copy of the License at
  *
- * This software is provided "as is"; the IDT or individuals within the IDT
- * cannot be held legally responsible for any problems the software may cause.
- * =============================================================================
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package net.iaeste.iws.api;
 
